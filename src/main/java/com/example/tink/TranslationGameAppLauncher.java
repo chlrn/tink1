4cfcd1c433d6b4bbe33a1220fc3d26758937a234
chlrn <104934124+chlrn@users.noreply.github.com>
2026-04-10 18:31:51 +0400

    Update TranslationGameAppLauncher.java

--- src/main/java/com/example/tink/TranslationGameAppLauncher.java
+++ src/main/java/com/example/tink/TranslationGameAppLauncher.java
@@ -9,19 +9,15 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class TranslationGameAppLauncher extends Application {
 
-    // Контекст Spring, используемый для запуска приложения
     private static ConfigurableApplicationContext context;
 
-    // Главный метод для запуска Spring Boot приложения
     public static void main(String[] args) {
         context = SpringApplication.run(TranslationGameAppLauncher.class, args);
         launch(args);
     }
 
-    // Метод для запуска JavaFX UI
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Получаем компонент TranslationGameApp из Spring контекста
         TranslationGameApp translationGameApp = context.getBean(TranslationGameApp.class);
         translationGameApp.start(primaryStage);
     }
